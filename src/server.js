d5d1bbecf864269070fedcaba5ad7009c35bfd20
Noma <wiostz98kr@gmail.com>
2021-06-29 20:49:24 +0900

    Add morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,17 @@
 import express from "express";
-const app = express();
+import morgan from "morgan";
 
-console.log("Hellow, world!");
+const PORT = 4000;
+
+const app = express();
+const loggerMiddleware = morgan('dev');
+const handleHome = (req, res) => {
+    res.send("home");
+}
+
+app.use(loggerMiddleware);
+app.get("/", handleHome);
+
+const handleListening = () => console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
+app.listen(4000, handleListening);//서버가 시작될 때 작동하는 함수
+
