e983895f1fb437479601237ba4fc51a2bc2c11aa
Noma <wiostz98kr@gmail.com>
2021-07-01 23:10:07 +0900

    Add join feature using bcrypt

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,17 @@
+import User from '../models/User';
+
 export const getJoin = (req, res) => res.render("screens/join");
-export const postJoin = (req, res) => {
-    console.log(req.body);
-    res.send("Submited!!");
+export const postJoin = async (req, res) => {
+    const { userId, pwd } = req.body;
+    const idExists = await User.exists({ userId });
+    if (idExists) {
+        return res.status(400).render("screens/join", { error: "이미 존재하는 아이디입니다." });
+    }
+    await User.create({
+        userId,
+        pwd,
+    });
+    res.status(201).redirect("/login");
 }
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
 
 const userSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
+    userId: { type: String, required: true, unique: true },
     pwd: { type: String, required: true }
 })
+userSchema.pre("save", async function () {
+    this.pwd = await bcrypt.hash(this.pwd, 5);
+})
+
 const User = mongoose.model("User", userSchema);
 export default User;
