8d3b0c6063049ec84276ea3b2a71bc1ab803c018
Noma <wiostz98kr@gmail.com>
2021-07-01 17:56:20 +0900

    Solve DeprecationWarning

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017/review');
+mongoose.connect('mongodb://127.0.0.1:27017/review', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 const db = mongoose.connection;
 
 const handleError = (err) => console.log("DB Error", err);
