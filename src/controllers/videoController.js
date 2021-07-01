fb40fcdf5a195c5646e201e6d26f0208cc93cd1f
Noma <wiostz98kr@gmail.com>
2021-07-01 14:28:14 +0900

    Structure view templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const home = (req, res) => res.render("screens/home", { title: "home!!", message: "This is home!", videos: [{ title: "video1", rating: 4 }, { title: "video2", rating: 3 }] });
 export const see = (req, res) => res.send("Watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
-import { trending, search } from "../controllers/videoController";
+import { home, search } from "../controllers/videoController";
 
 const rootRouter = express.Router();
-rootRouter.get("/", trending);
+rootRouter.get("/", home);
 rootRouter.get("/join", join);
 rootRouter.get("/login", login);
 rootRouter.get("/search", search);
