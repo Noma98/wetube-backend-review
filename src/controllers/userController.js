186311753174cdc7a42bbb6fa525fd82b4918a7c
Noma <wiostz98kr@gmail.com>
2021-07-02 15:55:22 +0900

    Add logout feature

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -27,10 +27,12 @@ export const postLogin = async (req, res) => {
     }
     req.session.loggedIn = true;
     req.session.user = findUser;
-    return res.redirect("/");
+    return res.status(200).redirect("/");
 };
-
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.status(200).redirect("/");
+}
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getJoin, getLogin, postLogin, postJoin } from "../controllers/userController";
+import { getJoin, getLogin, postLogin, postJoin, logout } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.get("/logout", logout);
 rootRouter.get("/search", search);
 
 export default rootRouter;
